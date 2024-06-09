063887914b29043f4d5184a7dbd20e0447fc7bba
VladislavKhodoruk <xodoruk@mail.ru>
2024-06-10 02:14:22 +0300

    highlighter

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // Hello, world![1] => Hello, world!
 
 function removeWikipediaLinks(text){
+  event.preventDefault();
   let newStr = '';
   let flag = false;
   for(let i = 0; i < text.length; i++){
@@ -22,10 +23,10 @@ function removeWikipediaLinks(text){
 }
 
 //shortcut
-let rwl = removeWikipediaLinks;
+//let rwl = removeWikipediaLinks;
 
 //Automated version. Enabled by default
-//Select the text, click 'c' and formatted text will be copied to the clipboard
+//Select the text, click ctrl + 'c' and formatted text will be copied to the clipboard
 textSelectionTemplate(removeWikipediaLinks, 'cс');
 
 
@@ -35,6 +36,7 @@ textSelectionTemplate(removeWikipediaLinks, 'cс');
 // 'QweRty' => 'qwerty'
 
 function stringToLowerCase(text) {
+  event.preventDefault();
   copyToClipboard(text.toLowerCase());
 }
 
@@ -42,14 +44,14 @@ function stringToLowerCase(text) {
 let stlc = stringToLowerCase;
 
 //Automated version. Enabled by default
-//Select the text, click 'l' and text in lower case will be copied to the clipboard
+//Select the text, click ctrl + 'l' and text in lower case will be copied to the clipboard
 textSelectionTemplate(stringToLowerCase, 'lд');
 
 
 
 // <<<------------------3------------------>>>
 // Removes element from the DOM tree
-// Hover your mouse on element and click 'Backspace'
+// Hover your mouse on element and click 'Delete'
 
 function removeElement() {
   let hoveredElement = null;
@@ -100,6 +102,145 @@ let rewh = removeElementWithHighlight;
 //Disabled by default
 
 
+
+// <<<------------------5------------------>>>
+// Highlights selected text
+
+function highlightSelectedText(style){
+  var userSelection = window.getSelection().getRangeAt(0);
+  var safeRanges = getSafeRanges(userSelection);
+  for (var i = 0; i < safeRanges.length; i++) {
+    highlightRange(safeRanges[i]);
+  }
+
+  function highlightRange(range) {
+    var newNode = document.createElement("div");
+    newNode.setAttribute(
+      "style",
+      style
+    );
+   newNode.setAttribute(
+      "class",
+      "highlighted-div"
+    );
+    range.surroundContents(newNode);
+  }
+  
+  function getSafeRanges(dangerous) {
+    var a = dangerous.commonAncestorContainer;
+    // Starts -- Work inward from the start, selecting the largest safe range
+    var s = new Array(0), rs = new Array(0);
+    if (dangerous.startContainer != a) {
+      for (var i = dangerous.startContainer; i != a; i = i.parentNode) {
+        s.push(i);
+      }
+    }
+    if (s.length > 0) {
+      for (var i = 0; i < s.length; i++) {
+        var xs = document.createRange();
+        if (i) {
+          xs.setStartAfter(s[i - 1]);
+          xs.setEndAfter(s[i].lastChild);
+        } else {
+          xs.setStart(s[i], dangerous.startOffset);
+          xs.setEndAfter((s[i].nodeType == Node.TEXT_NODE) ? s[i] : s[i].lastChild);
+        }
+        rs.push(xs);
+      }
+    }
+  
+    // Ends -- basically the same code reversed
+    var e = new Array(0), re = new Array(0);
+    if (dangerous.endContainer != a) {
+      for (var i = dangerous.endContainer; i != a; i = i.parentNode) {
+        e.push(i);
+      }
+    }
+    if (e.length > 0) {
+      for (var i = 0; i < e.length; i++) {
+        var xe = document.createRange();
+        if (i) {
+          xe.setStartBefore(e[i].firstChild);
+          xe.setEndBefore(e[i - 1]);
+        } else {
+          xe.setStartBefore((e[i].nodeType == Node.TEXT_NODE) ? e[i] : e[i].firstChild);
+          xe.setEnd(e[i], dangerous.endOffset);
+        }
+        re.unshift(xe);
+      }
+    }
+  
+    // Middle -- the uncaptured middle
+    if ((s.length > 0) && (e.length > 0)) {
+      var xm = document.createRange();
+      xm.setStartAfter(s[s.length - 1]);
+      xm.setEndBefore(e[e.length - 1]);
+    } else {
+      return [dangerous];
+    }
+  
+    // Concat
+    rs.push(xm);
+    console.log('RS');
+    for(let i  = 0; i < rs.length; i++ ){
+     console.log(rs[i]);
+    }
+    response = rs.concat(re);
+    console.log('RESPONSE');
+    for(let i  = 0; i < rs.length; i++ ){
+     console.log(response[i]);
+    }
+  
+    // Send to Console
+    return response;
+  }
+
+}
+
+function toggleHighlightedDivsAuto(keysForHighlight, keysForUnhighlight){
+  let selectedText;
+  let timer;
+  
+  document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    const isCtrlPressed = event.ctrlKey;
+    if(isCtrlPressed){
+      if (keysForHighlight.includes(key)) {
+        event.preventDefault();
+        highlightSelectedText("background-color: aqua; display: inline;");
+      }
+
+      if (keysForUnhighlight.includes(key)) {
+        event.preventDefault();
+        if (timer) {
+          clearTimeout(timer);
+          timer = null;
+          const highlightedDivs = document.getElementsByClassName("highlighted-div");
+          const arr = Array.prototype.slice.call(highlightedDivs);
+          for (var i = 0; i < arr.length; i++) {
+            arr[i].setAttribute(
+              "style",
+              "display: inline;"
+            );
+          }
+        } else {
+          timer = setTimeout(function() {
+            timer = null;
+          }, 500);
+        }
+      } 
+    }
+  });
+}
+
+//Automated version. Enabled by default
+// Select text and click ctrl + 'h' to highlight
+// Click ctrl + 'u' to cancel for previous highlighted text (NOT IMPLEMENTED YET!!!)
+// Click ctrl + double click 'u' to cancel for all highlighted text
+toggleHighlightedDivsAuto('hр', 'uг');
+
+
+
 // <<<------------DEPENDENCIES------------>>>
 
 function copyToClipboard(text) {
@@ -121,7 +262,8 @@ function textSelectionTemplate(userFunc, keys){
 
   document.addEventListener('keydown', function(event) {
     const key = event.key;
-    if (keys.includes(key.toLowerCase()) && selectedText !== '') {
+    const isCtrlPressed = event.ctrlKey;
+    if (keys.includes(key.toLowerCase()) && selectedText !== '' && isCtrlPressed) {
       userFunc(selectedText);
     }
   });
